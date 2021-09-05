ad3dd9b5ff9f9f31df5a549d76109be03adac3dc
Yander Sanchez <sanchezyander@gmail.com>
2021-09-05 01:09:45 -0400

    introduction to dart

--- sample/bin/sample.dart
+++ sample/bin/sample.dart
@@ -1,4 +1,5 @@
 import 'package:sample/sample.dart' as sample;
+import "dart:math";
 
 // void main(List<String> arguments) {
 //   print('Hello world: ${sample.calculate()}!');
@@ -8,7 +9,101 @@ import 'package:sample/sample.dart' as sample;
 // https://courses.codewithandrea.com/
 
 void main(List<String> arguments) {
+  classOne();
+  classTwo();
+  classThree();
+  classFour();
+  classFive();
+}
+
+void classFive() {
+//   Clousures, fold method, Enums, 
+//   sum using fold method
+  final numbersSum2 = [1, 2, 3, 4];
+  print("The sum2 is ${sum2(numbersSum2)}");
+//   Using enums
+  print("Using enums");
+  printError(NetworkError.badURL);
+}
+
+void classFour(){
+  //   List, Map, Generics, If-else, for, while
+  //   List, Map
+  //   List
+  print("class four: List, Map, Generics, If-else, for, while");
+  print("List");
+  var primeNumbers = [2, 3, 5, 7, 9];
+  primeNumbers.addAll([11, 13, 17, 19]);
+  primeNumbers.add(23);
+  print(primeNumbers);
+  // prefer classes to maps for data models
+  // map -> used to represent JSON data
+  print("Map");
+  var person = {
+    "name": "Yander",
+    "age": 22,
+    "height": 1.83
+  };
+  person["weight"] = 65; // Kg
+  print(person["name"]);
+  print(person["weight"]);
+  //   Generics
+  print("Generics");
+  //   deprecated List<int>();
+  var primeNumbers2 = <int>[];
+  primeNumbers2.addAll([2, 3, 5, 7, 9]);
+  var person2 = <String, dynamic>{
+    "name": "Yander",
+    "age": 22,
+    "height": 1.83
+  };
+  print(primeNumbers2);
+  print(person2["age"]);
+  print("Odd - Even");
+  
+  if(getOddEven(5)) {
+    print("The number is even.");
+  } else {
+    print("The number is odd.");
+  }
+//   Ternary operator
+  final type = (getOddEven(7) ? "Is even" : "Is odd");
+  print(type);
+  
+//   While Loop
+  final numbers = [1, 2, 3, 4, 5, 6];
+  final result = sum(numbers);
+  print("The sum of ${numbers} is ${result}.");
+  
+//   For Loop
+  int num = 3;
+  int times = 3;
+  final multiply = multiplyNumberMany(num, times);
+  print("The number ${num} multiply by itself ${times} times is ${multiply}.");
+} 
+
+void classThree() {
+  // Mixins
+  print("classTwo: Mixins");
+  final personMixin = PersonMixin(name: "Yander", age: 22, height: 1.84, weight: 145);
+//   using mixin
+  print("The BMI is ${personMixin.bmi}");
+}
+
+void classTwo() {
+  // Abstract class
+  //   final shape = Shape(); // Error can not instanciate it.
+  print("classTwo: Abstract classes");
+  final square = Square(side: 22.2);
+  print("The area of squeare is ${square.area}.");
+  print("The area of squeare is ${printArea(square)}.");
+  final circle = Circle(radius: 4.6);
+  print("The area of circle is ${printArea(circle)}.");
+}
+
+void classOne() {
 //   Variable declaration | variable initialization
+print("classOne: Base");
   String name             = 'Yander';
   int age                 = 22;
   double height           = 1.84;
@@ -51,7 +146,6 @@ void main(List<String> arguments) {
   print(employee.describe());
   print(employee.toString());
   print(employee);
-  
 }
 
 class Person {
@@ -94,5 +188,96 @@ String ageInTenMoreYears({int age = 0, String name = ""}) {
   return onFuture;
 }
 
+// Class Two
 
+double printArea(Shape shape) {
+  return shape.area;
+}
 
+abstract class Shape {
+//   computed property
+  double get area;
+}
+
+class Square implements Shape {
+  Square({this.side = 0});
+  final double side;
+  double get area => side * side;
+}
+
+class Circle implements Shape {
+  Circle({this.radius = 0});
+  final double radius;
+  double get area => radius * radius * pi;
+}
+
+// Mixins
+
+  class PersonMixin with BMI{
+    PersonMixin({this.name = "", this.age = 0, this.height = 0, this.weight = 0});
+    final String name;
+    final int age;
+    final double height;
+    final double weight;
+    // computed property
+    double get bmi => calculateBMI(weight, height);
+  }
+
+mixin BMI {
+  double calculateBMI(double weight, double height) {
+    return weight / (height * height);
+  }
+}
+
+// Class four : List, Map, Generics, If-else, for, while
+
+bool getOddEven(int number) {
+  if (number % 2 == 0) 
+    return true;
+  else return false;
+}
+
+int sum(List<int> numbers) {
+  int i = 0;
+  int result = 0;
+  while(i < numbers.length) {
+    result += numbers[i];
+    i++;
+  }
+  return result;
+}
+
+int multiplyNumberMany(int value, int times) {
+  int result = 0;
+  for (int i = 0; i < times; i++) {
+    result += value * value;
+  }
+  return result;
+}
+
+// Class five : Clousures, fold method, Enums, 
+
+int sum2(List<int> numbers) {
+  return numbers.fold(0, (result, value) => result + value);
+}
+
+enum NetworkError {
+  badURL,
+  timeout,
+  resourseNotAvailable,
+}
+
+void printError(NetworkError error) {
+  switch (error) {
+    case NetworkError.badURL:
+      print("Bad url.");
+      break;
+    case NetworkError.timeout:
+      print("Time out.");
+      break;
+    case NetworkError.resourseNotAvailable:
+      print("Resourse not available.");
+      break;
+      
+  }
+}
